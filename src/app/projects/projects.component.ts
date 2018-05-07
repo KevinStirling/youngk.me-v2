36b2c36056251348877399383803100e2357ecaf
KevinStirling <youngks93@gmail.com>
2018-05-06 22:22:05 -0400

    change location of images

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -13,14 +13,14 @@ export class ProjectsComponent implements OnInit {
   constructor() {
     this.projects = [
       {
-        Image: '../assets/project-images/nycbike.png',
+        Image: '../assets/nycbike.png',
         Name: 'NYC Bike',
         Description: 'NYC Bike is a tool to view the status of Citi bike stations in NYC, built using d3 and SVG. ' +
         'Built initially to learn the basics of d3, but there are plans to expand on this concept in the future!',
         Link: 'https://kevinstirling.github.io/NewYorkCitiBikeTracker/'
       },
       {
-        Image: '../assets/project-images/meta-jersey.png',
+        Image: '../assets/meta-jersey.png',
         Name: 'Meta-Jersey',
         Description: 'Meta-Jersey provides a tool for journalists that can aid them in understanding what is causing ' +
         'shifts in the New Jersey census. ' +
@@ -29,7 +29,7 @@ export class ProjectsComponent implements OnInit {
         Link: 'http://pranavrele.com/meta-jersey-hosting/'
       },
       {
-        Image: '../assets/project-images/omnia.png',
+        Image: '../assets/omnia.png',
         Name: 'Omnia',
         Description: 'Omnia Labs is a small group of friends who met while attending Montclair State University,' +
         'who decided to create apps and programs, both for practice, and fun.',
